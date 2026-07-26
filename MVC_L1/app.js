f8bc09337da72f1ade55160c1d9cdb3d44eaf42d
JeyaVardhan-L <68065109+JeyaVardhan-L@users.noreply.github.com>
2026-07-26 23:57:59 +0530

    Updated again

--- MVC_L1/app.js
+++ MVC_L1/app.js
@@ -1,18 +1,11 @@
-// app.js
-
 const express = require('express');
 const app = express();
-
-// Tell Express to use EJS as our View Engine
 app.set('view engine', 'ejs');
 
-// Import the Controller
 const { showMessage } = require('./controllers/messageController');
 
-// When the user goes to the home page ('/'), run the showMessage function
 app.get('/', showMessage);
 
-// Start the server on port 3000
 app.listen(3000, () => {
     console.log('Server is running on http://localhost:3000');
 });
