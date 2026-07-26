83a8d9295d36601a81059220aed8d55a2e144e4e
JeyaVardhan-L <68065109+JeyaVardhan-L@users.noreply.github.com>
2026-07-26 23:56:18 +0530

    Updated controller

---
+++ MVC_L1/controllers/messageController.js
@@ -0,0 +1,8 @@
+const { getMessage } = require('../models/messageModel');
+function showMessage(req, res) {
+
+    const data = getMessage();
+    res.render('view', { message: data });
+}
+
+module.exports = { showMessage };
