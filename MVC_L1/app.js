66e1ac3cc261e814e946a2e3a82cc8bdc7edde4e
JeyaVardhan-L <68065109+JeyaVardhan-L@users.noreply.github.com>
2026-07-26 23:59:04 +0530

    Modified the app.js now

--- MVC_L1/app.js
+++ MVC_L1/app.js
@@ -1,11 +1,8 @@
 const express = require('express');
 const app = express();
 app.set('view engine', 'ejs');
-
 const { showMessage } = require('./controllers/messageController');
-
 app.get('/', showMessage);
-
 app.listen(3000, () => {
     console.log('Server is running on http://localhost:3000');
 });
