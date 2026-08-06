46762a00ba82ddca0c39677501b380299f15559d
JeyaVardhan-L <68065109+JeyaVardhan-L@users.noreply.github.com>
2026-08-06 23:53:10 +0530

    Updated the initial paths

---
+++ script.js
@@ -0,0 +1,7 @@
+// 1. Find the button in the HTML and save it in a variable
+const button = document.getElementById("myBtn");
+button.addEventListener("click", function() {
+    
+    // 3. When clicked, do this:
+    alert("Ouch! You clicked me!");
+});
